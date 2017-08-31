13e96ed745ea4332629bef847f4087dee79e318d
agliullin <ratmir-91@mail.ru>
2017-08-31 17:06:54 +0300

    With Handler

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,12 @@
+import models.Handler;
+
+import java.io.IOException;
 import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
         Server server = new Server(2000);
         server.startServer();
-//        Random rand=new Random();
-//        Integer a = rand.nextInt(10);
-//        System.out.println(a);
+
     }
 }
